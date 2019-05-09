3787023083c42097deac250b52e508dfabe398f0
jobo90 <joelbock1@gmail.com>
2019-05-09 12:07:18 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
+          <Navigation />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={Error} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
